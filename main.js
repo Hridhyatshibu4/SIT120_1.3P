27ef03e4004569ed087510fbb2f2357472496042
Hridhyatshibu4 <106868488+Hridhyatshibu4@users.noreply.github.com>
2023-09-17 01:50:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        resume: {
+          name: "Hridhya Thuruthippillil Shibu",
+          address: "13 Peacock Street, Burwood, 3125",
+          tel: "0476117192",
+          email: "hridhyatshibuh4@gmail.com",
+          linkedin: "linkedin.com/in/hridhyatshibuh4",
+          careerProfile: "A young and enthusiastic mind prepared to confront emerging challenges...",
+          // Add more resume details as needed
+        },
+      };
+    },
+  });
+  
+  app.mount("#app");
+  
